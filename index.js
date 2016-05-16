a6bda2cc548a87d08a45350bf45d9a6e13bb401a
yongjz <yjz_1008@163.com>
2016-05-17 00:38:03 +0800

    将material-ui和react更新到最新版本

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ import Login from './src/js/Login';
 import Signup from './src/js/Signup';
 import QrCodeGridList from './src/js/component/QrCodeGridList';
 
-//Some components use react-tap-event-plugin to listen for touch events. 
-//This dependency is temporary and will go away once react v1.0 is released. 
+//Some components use react-tap-event-plugin to listen for touch events.
+//This dependency is temporary and will go away once react v1.0 is released.
 //Until then, be sure to inject this plugin at the start of your app.
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
